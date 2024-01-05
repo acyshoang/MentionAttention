8d4587e7ba1984fddf863cb216f55051570558d9
acyshoang <trunghoang1124@gmail.com>
2024-01-05 13:21:20 +0700

    Done

--- main.py
+++ main.py
@@ -1,15 +1,27 @@
 import discord
+from discord.ext import commands
 
-discord_token = "MTE5MjY2MjgyMDc1MjQ1NzgyOA.GZWuLN.kOFbWk9Gf4E6PuvzQKoSNORVNIFQeY2nL2m86I"
+discord_token = "MTE5MjY2MjgyMDc1MjQ1NzgyOA.GfT_AS.43aKe7z2z0bOGscnvFoXTTExB91wde4jN8-aKo"
 
 intents = discord.Intents.all()
-bot = discord.Bot(command_prefix="mention!")
+bot = commands.Bot(command_prefix="mention!", intents=intents)
+
+looping = False
 
 @bot.command()
-async def test(ctx):
-    await ctx.send("@everyone hi!")
+async def start(ctx):
+    global looping
+    looping = False
+    while not looping:
+        await ctx.send("@everyone hi!")
+
 @bot.command()
-async def help(ctx):
+async def stop(ctx):
+    global looping
+    looping = True
+
+@bot.command()
+async def helpme(ctx):
     messages = """
 ```
 mention!help - Show all available commands
@@ -20,6 +32,6 @@ mention!help - Show all available commands
 @bot.event
 async def on_ready():
     print("I'm ready to annoying everyone")
-    await bot.change_presence(activity=discord.activity(type=discord.ActivityType.playing, name = "mention!help"))
+    await bot.change_presence(activity=discord.Activity(type=discord.ActivityType.playing, name = "mention!help"))
 
 bot.run(discord_token)
